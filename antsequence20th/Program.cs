d374e7cb207b40735b07a242709c442dd29336ba
MoonHyekyung <moonsoft02@gmail.com>
2020-05-12 22:41:31 +0900

    개미수열 끝냈당~

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,7 +10,32 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
-           
+            String start = "1";
+            int count = 0;
+            Console.WriteLine("1번째 수열 : 1");
+            for (int i = 2; i <= 20; i++)
+            {
+                Console.Write(i + "번째 수열 : ");
+                String temp = start;
+                start = "";
+                char num = temp[0];
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (temp[j] == num)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        start += num + "" + count;
+                        count = 1;
+                        num = temp[j];
+                    }
+                }
+                start += num + "" + count;
+                count = 0;
+                Console.WriteLine(start);
+            }
         }
     }
 }
